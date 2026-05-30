82b1390f3e9831b652d7f5c098a5f14c566256c5
nazmuloslam1-tech <nazmuloslam1@gmail.com>
2026-05-30 18:50:42 +0200

    feat: add Deactivator class (runs on plugin disable)

---
+++ includes/Deactivator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Refundia;
+
+defined('ABSPATH') || exit;
+
+class Deactivator {
+    public static function deactivate() {
+        // Will tear down webhooks, etc. in later phases
+    }
+}
