2805da0a2c05c2ae952d731f6e8b49d6350e12a5
nazmuloslam1-tech <nazmuloslam1@gmail.com>
2026-05-30 20:08:12 +0200

    feat(sync): wire Sync::init() in Plugin bootstrap

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -7,6 +7,7 @@ class Plugin {
 
     public function __construct() {
         new RestRoutes();
+        Sync::init();
         add_action('admin_menu',  [$this, 'add_admin_menu']);
         add_action('admin_post_refundia_pair',   [$this, 'handle_pair_request']);
         add_action('admin_post_refundia_unpair', [$this, 'handle_unpair_request']);
