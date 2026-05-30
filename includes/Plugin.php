bee1c2b4c0116ab39d8c598497f0c3e27d15a318
nazmuloslam1-tech <nazmuloslam1@gmail.com>
2026-05-30 20:22:41 +0200

    feat(refund): wire RefundHandler in Plugin bootstrap

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -7,6 +7,7 @@ class Plugin {
 
     public function __construct() {
         new RestRoutes();
+        new RefundHandler();
         Sync::init();
         add_action('admin_menu',  [$this, 'add_admin_menu']);
         add_action('admin_post_refundia_pair',   [$this, 'handle_pair_request']);
